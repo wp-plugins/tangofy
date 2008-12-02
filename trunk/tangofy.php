d844852c089d925f336860896821f0e1e008c74d
demetris <demetris@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-02 18:12:20 +0000

    v. 0.0.6
    git-svn-id: https://plugins.svn.wordpress.org/tangofy@77071 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/tangofy.php
+++ trunk/tangofy.php
@@ -5,7 +5,7 @@
 Plugin Name:    Tangofy
 Plugin URI:     http://op111.net/p65
 Description:    Tangofies 16x16 icons and hides 32x32 icons in the administration area of WordPress 2.7.
-Version:        0.0.5
+Version:        0.0.6
 Author:         Demetris
 Author URI:     http://op111.net/
 
@@ -24,29 +24,10 @@ Source:         http://tango.freedesktop.org/
 Licence:        http://creativecommons.org/licenses/by-sa/2.5/
 
 ------------------------------------------------------------------------------
-CHANGELOG
-
-0.0.5   2008-12-01
-    +   Added readme.txt for wordpress.org.
-0.0.4   2008-12-01
-    +   Replaced accessories-text-editor (Tango) with accessories-text-editor (GNOME) for Posts.
-0.0.3   2008-11-29
-    +   Trying scissors+triangle icon (GNOME) for Appearance, and extensions icon (Firefox) for Plugins.
-    +   Synced with WP CSS to reflect r9957 (http://trac.wordpress.org/changeset/9957).
-    +   Thanks to Josh Wood (http://joshix.com/) for feedback and suggestions.
-0.0.2   2008-11-28
-    +   Dropped echo to use wp_enqueue_style().  Thanks to Viper007Bond.
-0.0.1   2008-11-27
-    +   Replaced emblem-symbolic-link (GNOME) with web-browser (GNOME) for Links.
-0.0.0   2008-11-26
-    +   Put together.
-    +   Thanks to filosofo (http://www.ilfilosofo.com/) and Viper007Bond (http://www.viper007bond.com/) for suggesting the idea.
-
-------------------------------------------------------------------------------
 TO DO
 
-    +   Replace generic.png (see: wp-admin/includes/plugin.php).
-    +   Add options page.
++   Replace generic.png (see: wp-admin/includes/plugin.php).
++   Add options page.
 
 */
 
