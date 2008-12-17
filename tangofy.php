1160ae38381c63fac1039475acd5d467585e4e14
demetris <demetris@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-17 15:23:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tangofy/tags/0.1.5@80718 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tangofy.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+
+Plugin Name:    Tangofy
+Plugin URI:     http://op111.net/p65
+Description:    Better icons for the admin menu of WordPress 2.7:  Select an included icon set (Fugue, Silk, Tango, Tango 2) or add your own!
+Version:        0.1.5
+Author:         Demetris
+Author URI:     http://op111.net/
+
+Licence:        http://www.gnu.org/licenses/gpl-2.0.html
+
+FUGUE ICONS
+Source:         http://www.pinvoke.com/
+Licence:        http://creativecommons.org/licenses/by/3.0/
+
+GIMP ICONS
+Source:         http://gimp.org/
+Licence:        http://www.gnu.org/licenses/gpl-2.0.html
+
+GNOME ICONS
+Source:         http://ftp.gnome.org/pub/GNOME/desktop/
+Licence:        http://www.gnu.org/licenses/gpl-2.0.html
+
+PIDGIN ICONS
+Source:         http://pidgin.im/
+Licence:        http://www.gnu.org/licenses/gpl-2.0.html
+
+SILK ICONS
+Source:         http://www.famfamfam.com/lab/icons/silk/
+Licence:        http://creativecommons.org/licenses/by/2.5/
+
+TANGO ICONS
+Source:         http://tango.freedesktop.org/
+Licence:        http://creativecommons.org/licenses/by-sa/2.5/
+
+------------------------------------------------------------------------------
+TO DO
+
++   Replace generic.png (see: wp-admin/includes/plugin.php).
++   Add more options to Options.
+
+DONE
+
++   Add options page. (v. 0.1.0, 2008-12-05)
+
+*/
+
+/* Activation */
+function tangofy_actv() {
+	/* Create our option, set it to something. */
+	add_option( 'tangofy_iconset', 'tango', '', 'yes' );
+}
+
+/* Deactivation */
+function tangofy_dactv() {
+	delete_option( 'tangofy_iconset' );
+}
+
+/* Create admin menu and options page. */
+function tangofy_admin_ui() {
+	if ( function_exists('add_options_page') ) {
+		/* add_options_page(page_title, menu_title, access_level/capability, file, [function]) */
+		add_options_page( __( 'Tangofy Options', 'tangofy' ), __( 'Tangofy!', 'tangofy' ), 'manage_options', 'tangofy/tangofy-ui.php', '' );
+	}
+}
+
+/* Link to options page in active plugins list. */
+function tangofy_add_plugin_action_link($action_links, $file) {	
+	$plugin_self = plugin_basename(__FILE__);
+	if ( $file == $plugin_self ) {
+		$link = '<a href="' . admin_url('options-general.php?page=tangofy/tangofy-ui.php') . '">' . __('Settings') . '</a>';
+		array_unshift( $action_links, $link );
+	}
+	return $action_links;
+}
+
+/* Create a stylesheet URI, enqueue, and emit it. */
+function tangofy_admin_head() {
+	$ss = plugins_url() . '/tangofy/css/' . get_option('tangofy_iconset') . '.css';
+	wp_enqueue_style( 'tangofy_style', $ss, '', '', 'all' );
+	wp_print_styles( 'tangofy_style' );
+}
+
+/* Inform WP about activate/deactivate functions. */
+register_activation_hook( __FILE__, 'tangofy_actv' );
+register_deactivation_hook( __FILE__, 'tangofy_dactv' );
+/* For localization: load_plugin_textdomain(Name, xxx, /path/to/pofiles) */
+load_plugin_textdomain( 'tangofy', false, '/tangofy/languages' );
+/* Register the admin menu item/page function to be called. */
+add_action( 'admin_menu', 'tangofy_admin_ui' );
+/* Arrange for settings link to be inserted in plugin screen list. */
+add_filter( 'plugin_action_links', 'tangofy_add_plugin_action_link', 10, 2 );
+/* Arrange for the replacement stylesheet function to run. */
+add_action( 'admin_head', 'tangofy_admin_head' );
